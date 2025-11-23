5062fb657faf79926bebe08e5d000e42f09619d2
Allan Lewis <allan.e.lewis@gmail.com>
2025-11-23 12:21:40 -0500

    re-write

---
+++ arch-zsh/.config/zsh/os-arch.zsh
@@ -0,0 +1,6 @@
+# ~/.config/zsh/os-arch.zsh
+# Arch-specific Zsh customizations.
+# (Empty for now; add Arch-specific PATH tweaks, aliases, etc. later.)
+
+# Example placeholder:
+# export ARCH_EXAMPLE_VAR="arch"
