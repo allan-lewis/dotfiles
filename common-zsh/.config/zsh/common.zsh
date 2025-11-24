54573573b2ee18e49f27853708b782ed7222f0d6
Allan Lewis <allan.e.lewis@gmail.com>
2025-11-23 20:52:28 -0500

    starship

--- common-zsh/.config/zsh/common.zsh
+++ common-zsh/.config/zsh/common.zsh
@@ -1,6 +1,14 @@
 # ~/.config/zsh/common.zsh
 # Common interactive settings for all OSes.
 
+# Fix TERM when using Ghostty on hosts without xterm-ghostty terminfo
+if [[ "$TERM" = "xterm-ghostty" ]]; then
+  # If this terminal type isn't known here, fall back to a common one
+  if ! infocmp >/dev/null 2>&1; then
+    export TERM="xterm-256color"
+  fi
+fi
+
 # History settings
 HISTFILE="$HOME/.zsh_history"
 HISTSIZE=5000
